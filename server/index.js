0cae0c6ca62ddbfd5134922fb699fbbc9fdfb6e1
WAHYU FATUR RIZKI <wahyufaturrizkyy@gmail.com>
2022-08-12 18:40:22 +0700

    feat: add models and mongoose

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
+require("dotenv").config();
+
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const { errorHandler, notFound } = require("./handlers");
 
+const db = require("./models");
 const app = express();
-const port = 4000;
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => res.json({ helo: "hello world" }));
 
